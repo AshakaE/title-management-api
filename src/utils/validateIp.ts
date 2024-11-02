a47456f5cb33acdec9d8c9bcfcfaf455a21f0826
AshakaE <ashakaegerega@gmail.com>
2024-11-02 12:24:30 +0100

    Disable req-bans

--- src/utils/validateIp.ts
+++ src/utils/validateIp.ts
@@ -1,5 +1,6 @@
 import { clientIpValidator } from "req-scopes";
 
 export const validateIp = (ip: string) => {
-  return clientIpValidator(ip) && ip !== "::1" && ip !== "::ffff:127.0.0.1";
+//   return clientIpValidator(ip) && ip !== "::1" && ip !== "::ffff:127.0.0.1";
+  return ''
 };
